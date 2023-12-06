c3c34fc9f675a621cb2dd6a624edcc0a20dca935
Saba Ahmad <sabaahmad821@gmail.com>
2023-12-07 01:49:52 +0500

    Done

---
+++ Authentication_ADO/Controllers/AccountsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Authentication_ADO.Models;
+
+namespace Authentication_ADO.Controllers
+{
+    public class AccountsController : Controller
+    {
+        Account_DBEntities entity = new Account_DBEntities();
+
+        // GET: Accounts
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User UserInfo)
+        {
+            entity.Users.Add(UserInfo);
+            entity.SaveChanges();
+            return RedirectToAction("Login");
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel credentials)
+        {
+
+            bool UserExists = entity.Users.Any(x => x.Email == credentials.Email && x.Passcode == credentials.Password);
+            User u = entity.Users.FirstOrDefault(x => x.Email == credentials.Email && x.Passcode ==credentials.Password);
+            if(UserExists)
+            {
+                FormsAuthentication.SetAuthCookie(u.UserName, false);
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "Username or Password is wrong");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("Login");
+        }
+    }
+}
--- Authentication_ADO/Controllers/HomeController.cs
+++ Authentication_ADO/Controllers/HomeController.cs
@@ -6,8 +6,10 @@ using System.Web.Mvc;
 
 namespace Authentication_ADO.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
